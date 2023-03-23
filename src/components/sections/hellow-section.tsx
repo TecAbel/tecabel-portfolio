f1396c458ad1795d87d8f856273d744856c2de50
TecAbel <abelardo96.work@gmail.com>
2023-03-23 10:31:05 -0600

    feat(hellow.tsx): leadership p

--- src/components/sections/hellow-section.tsx
+++ src/components/sections/hellow-section.tsx
@@ -38,13 +38,21 @@ export const HellowSection: FC = () => {
       </p>
       <p className="text-justify">
         The last but not least,{' '}
-        <strong className="text-main-dark underline dark:text-main">NextJs</strong>,
-        my second favorite framework, It came up, to fix some difficulties of
-        the past two... SEO, the capavility to be find easily on web browsers,
+        <strong className="text-main-dark underline dark:text-main">
+          NextJs
+        </strong>
+        , my second favorite framework, It came up, to fix some difficulties of
+        the past two... SEO, the capavility to be finded easily on web browsers,
         just as Google, and some other things, with Server Side Rendering, a
-        complete different way to work. Perfect for static sites, just like this portfolio, or even better, marketplaces...
+        complete different way to work. Perfect for static sites, just like this
+        portfolio, or even better, marketplaces...
       </p>
-      <p></p>
+      <p>
+        With this kind of experience, and technical knowledge, confidence, a new
+        opportunity was right in front, <strong className='text-main-dark dark:text-main underline'>leadership</strong>, my largest
+        team was made by three front developers, developping a NextJs app, a
+        textile marketplace. Wich is about to be publicated...
+      </p>
     </div>
   )
 }
